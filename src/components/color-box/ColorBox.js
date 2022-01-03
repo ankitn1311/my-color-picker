c319cc730d5cee8fe39cbeb18dd87716ab761472
Ankit Negi <ankitn1311@gmail.com>
2022-01-03 15:50:48 +0530

    Github Pages Setup

---
+++ src/components/color-box/ColorBox.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Color from "../color/Color";
+import "./ColorBox.scss";
+
+const ColorBox = () => {
+  return (
+    <div className="ColorBox">
+      <Color />
+    </div>
+  );
+};
+
+export default ColorBox;
---
+++ src/components/color/Color.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Color.scss";
+
+const Color = () => {
+  return <div className="Color"></div>;
+};
+
+export default Color;
