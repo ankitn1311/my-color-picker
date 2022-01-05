f3bbdea651dc557326de23a3fb69eaa905042e7f
Ankit Negi <ankitn1311@gmail.com>
2022-01-05 12:44:30 +0530

    rgb values shown

--- src/components/selected-color/SelectedColor.js
+++ src/components/selected-color/SelectedColor.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useColors } from "../../contexts/ColorsContext";
 import "./SelectedColor.scss";
 
@@ -6,31 +6,35 @@ const SelectedColor = () => {
   const { selected } = useColors();
   const [rgb, setRgb] = useState(true);
 
-  const hslToRgb = (h, s, l) => {
-    var r, g, b;
-    if (s == 0) {
-      r = g = b = l;
+  const [colorValue, setColorValue] = useState(selected);
+
+  useEffect(() => {
+    let color = selected;
+    if (rgb) {
+      color = changeToRgb(selected);
+      setColorValue(color);
     } else {
-      var hue2rgb = function hue2rgb(p, q, t) {
-        if (t < 0) t += 1;
-        if (t > 1) t -= 1;
-        if (t < 1 / 6) return p + (q - p) * 6 * t;
-        if (t < 1 / 2) return q;
-        if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
-        return p;
-      };
-      var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
-      var p = 2 * l - q;
-      r = hue2rgb(p, q, h + 1 / 3);
-      g = hue2rgb(p, q, h);
-      b = hue2rgb(p, q, h - 1 / 3);
+      setColorValue(selected);
     }
-    return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
+  }, [rgb, selected]);
+
+  const hslToRgb = (h, s, l) => {
+    let a = s * Math.min(l, 1 - l);
+    let f = (n, k = (n + h / 30) % 12) =>
+      l - a * Math.max(Math.min(k - 3, 9 - k, 1), -1);
+    // return [f(0), f(8), f(4)];
+    return [Math.round(f(0) * 255), Math.round(f(8)), Math.round(f(4))];
   };
 
   const changeToRgb = (selected) => {
-    // hsl( , , )
-    selected.substr(3).split(",");
+    const hsl = selected.substr(3).split(",");
+    const hue = hsl[0].substr(1);
+    const saturation = hsl[1].substr(0, hsl[1].length - 1) / 100;
+    const lightness = hsl[2].substr(0, hsl[2].length - 2) / 100;
+
+    const rgbValues = hslToRgb(hue * 1, saturation, lightness);
+    console.log({ rgbValues });
+    return `rbg(${rgbValues[0]}, ${rgbValues[1]}, ${rgbValues[2]})`;
   };
 
   return (
@@ -38,12 +42,13 @@ const SelectedColor = () => {
       <div
         className="SelectedColor"
         style={{ backgroundColor: selected }}></div>
-      <p>{rgb ? changeToRgb(selected) : selected}</p>
-      RGB:{" "}
+      <p>{colorValue}</p>
+      RGB:
       <input
         type="checkbox"
         name="type"
         id="type"
+        checked={rgb}
         onChange={(e) => (e.target.checked ? setRgb(true) : setRgb(false))}
       />
     </>
